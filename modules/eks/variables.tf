a21a2bff5f8c54a889dd95cebda184a9d3d7ab07
pedrojpx <pjpxavier@gmail.com>
2024-03-27 19:02:48 -0300

    eks module initial impl

---
+++ modules/eks/variables.tf
@@ -0,0 +1,8 @@
+variable "prefix" {}
+variable "vpc_id" {}
+variable "cluster_name" {}
+variable "retention_in_days" {}
+variable "subnet_ids" {}
+variable "desired_size"{}
+variable "max_size"{}
+variable "min_size"{}
