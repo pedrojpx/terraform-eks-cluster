5653502e902ed4969c6caf08ce2c89418c7441a2
pedrojpx <pjpxavier@gmail.com>
2024-03-27 19:24:56 -0300

    main.tf calling modules

---
+++ main.tf
@@ -0,0 +1,16 @@
+module "new-vpc" {
+    source = "./modules/vpc"
+    prefix = var.prefix
+}
+
+module "eks" {
+    source = "./modules/eks"
+    prefix = var.prefix
+    vpc_id = module.new-vpc.vpc_id
+    cluster_name = var.cluster_name
+    retention_in_days = var.retention_in_days
+    subnet_ids = module.new-vpc.subnet_ids
+    desired_size = var.desired_size
+    max_size = var.max_size
+    min_size = var.min_size
+}
